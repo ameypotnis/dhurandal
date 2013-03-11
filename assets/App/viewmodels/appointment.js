f3274db72718896e2e4ce56548cdc84ddea70e69
ameypotnis <apotnis@equalexperts.com>
2013-03-11 17:32:07 +0530

    Dhurandal bases SPA patient book initial commit

---
+++ assets/App/viewmodels/appointment.js
@@ -0,0 +1,39 @@
+﻿define(function (require) {
+    var http = require('durandal/http'),
+        app = require('durandal/app');
+
+    return {
+        displayName: "Today's Appointment",
+        appointment: ko.observableArray([]),
+
+        search: ko.observable(""),
+        results: ko.observableArray([]),
+
+        activate: function () {
+            //the router's activator calls this function and waits for it to complete before proceding
+            var that = this;
+
+            return http.get('http://localhost:3002/appointments/20130311', {}, 'jsoncallback').then(function(response) {
+                that.appointment(response);
+            });
+        },
+        select: function(item) {
+            //the app model allows easy display of modal dialogs by passing a view model
+            //views are usually located by convention, but you an specify it as well with viewUrl
+            item.viewUrl = 'views/detail';
+            app.showModal(item);
+        },
+        canDeactivate: function () {
+            //the router's activator calls this function to see if it can leave the screen
+            return app.showMessage('Are you sure you want to leave this page?', 'Navigate', ['Yes', 'No']);
+        },
+        add: function () {
+            //the router's activator calls this function and waits for it to complete before proceding
+            var that = this;
+
+            return http.get('http://localhost:3002/patients/search/'+that.search(), {}, 'jsoncallback').then(function(response) {
+                that.results(response);
+            });
+        }
+    };
+});
